efec0b99a8986df0121b276f4087dce26408d666
Maroun Melhem <maroun.melhem@gmail.com>
2018-08-22 22:00:06 +0300

    going back to dev branch to work on withMall version

---
+++ withoutMall/Classes/CameneosID.java
@@ -0,0 +1,22 @@
+/**
+ * Class Name: CameneosID
+ * Class Desc: 
+ * - Provide unique ID for Cameneos
+ *
+ * @version 1.0
+ * @package: Accov-tpFinal-2018-10504F
+ * @developer: Maroun Melhem <https://maroun.me>
+ * @author: Maroun Melhem <https://maroun.me>
+ */
+package Classes;
+
+public class CameneosID {
+    private final int id;
+    public CameneosID(int id)
+    {
+        this.id=id;
+    }
+    public String toString(){
+        return "Cameneon is: "+this.id;
+    }
+}
---
+++ withoutMall/Classes/Play.java
@@ -0,0 +1,33 @@
+/**
+ * Class Name: Play
+ * Class Desc: 
+ * - Play methods
+ *
+ * @version 1.0
+ * @package: Accov-tpFinal-2018-10504F
+ * @developer: Maroun Melhem <https://maroun.me>
+ * @author: Maroun Melhem <https://maroun.me>
+ */
+
+package Classes;
+
+public class Play {
+
+    //All color groups
+    public static Color[] allColors=
+    {
+        Color.RED,
+        Color.BLUE
+    };
+
+    //Cameneon objects
+    public static Cameneons[] cameneonsGroup=new Cameneons[allColors.length];
+    public static void main(String args[]){
+        Mall currentMall=new Mall();
+        for(int i =0;i<allColors.length;i++){
+            cameneonsGroup[i]=new Cameneons(currentMall,new CameneosID(i),allColors[i]);
+            new Thread(cameneonsGroup[i]).start();
+        }
+    }
+    
+}
